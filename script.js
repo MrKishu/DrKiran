52a5f2885610826a4b7565297e0c48ddc6dd9e42
Kishan N K <kishannk006@gmail.com>
2025-12-25 12:57:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ form.addEventListener("submit", async (e) => {
     message: formData.get("message")
   };
 
-  const res = await fetch("https://api-backend-api.vercel.app/api/contact", {
+  const res = await fetch("https://dr-kiran-api.vercel.app/api/contact", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
@@ -45,6 +45,7 @@ form.addEventListener("submit", async (e) => {
     alert("Error: " + (result.error || "Something went wrong"));
   }
 });
+
 
 
 
